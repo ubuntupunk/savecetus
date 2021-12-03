8a90c34dad76f0b50ee41ddda5f4cedbe7fffcdf
ubuntupunk <ubuntpunk@gmail.com>
2021-12-03 18:22:53 +0200

    second commit

--- app/src/main/java/com/example/savecetus/MapsActivity.kt
+++ app/src/main/java/com/example/savecetus/MapsActivity.kt
@@ -41,8 +41,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val capetown = LatLng(-33.918861, 18.423300)
+        mMap.addMarker(MarkerOptions().position(capetown).title("Marker in Cape Town"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(capetown))
     }
 }
